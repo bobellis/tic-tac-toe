f2b903888e8fe56fce40a83875bd3a6a2d8283ae
Peter & Jesse James <student@epicodus.com>
2015-06-03 11:02:57 -0700

    determines winner or tie and switches player turn

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+function Player(mark, isTurn) {
+  this.mark = mark;
+  this.isTurn = isTurn;
+}
+
+
+function Board() {
+  this.spaces = [];
+}
+
+Board.prototype.populate = function() {
+  for (var i = 1; i <= 3; i++ ) {
+    var space = new Space(1,i);
+    this.spaces.push(space);
+  };
+  for (var i = 1; i <= 3; i++ ) {
+    var space = new Space(2,i);
+    this.spaces.push(space);
+  };
+  for (var i = 1; i <= 3; i++ ) {
+    var space = new Space(3,i);
+    this.spaces.push(space);
+  };
+
+};
+
+function Space(xCoordinate, yCoordinate){
+  this.xCoordinate = xCoordinate;
+  this.yCoordinate = yCoordinate;
+  this.marked = "";
+  this.coordinates = [xCoordinate,yCoordinate];
+}
+
+Space.prototype.markedBy = function(player) {
+    var markedSpace =  player.mark;
+    this.marked = markedSpace;
+    return this.marked;
+};
+
+
+function Game() {
+  this.winner = "";
+}
+
+
+
+Game.prototype.switchPlayer = function(player1, player2) {
+
+  if (player1.isTurn === true) {
+    player1.isTurn = false;
+    player2.isTurn = true;
+  } else {
+    player1.isTurn = true;
+    player2.isTurn = false;
+  }
+};
+
+
+Game.prototype.determineWinner = function(board) {
+  var finalWinner = "";
+  if (board.spaces[0].marked === board.spaces[1].marked  && board.spaces[1].marked === board.spaces[2].marked) {
+    finalWinner = board.spaces[0].marked;
+
+  } else if (board.spaces[3].marked === board.spaces[4].marked  && board.spaces[4].marked === board.spaces[5].marked) {
+    finalWinner = board.spaces[3].marked;
+
+  } else if (board.spaces[6].marked === board.spaces[7].marked  && board.spaces[7].marked === board.spaces[8].marked) {
+    finalWinner = board.spaces[6].marked;
+
+  } else if (board.spaces[0].marked === board.spaces[4].marked  && board.spaces[4].marked === board.spaces[8].marked) {
+    finalWinner = board.spaces[0].marked;
+
+  } else if (board.spaces[6].marked === board.spaces[4].marked  && board.spaces[4].marked === board.spaces[2].marked) {
+    finalWinner = board.spaces[6].marked;
+
+  } else if (board.spaces[0].marked === board.spaces[3].marked  && board.spaces[3].marked === board.spaces[6].marked) {
+    finalWinner = board.spaces[0].marked;
+
+  } else if (board.spaces[1].marked === board.spaces[4].marked  && board.spaces[4].marked === board.spaces[7].marked) {
+    finalWinner = board.spaces[1].marked;
+
+  } else if (board.spaces[2].marked === board.spaces[5].marked  && board.spaces[5].marked === board.spaces[8].marked) {
+    finalWinner = board.spaces[2].marked;
+
+  } else {
+    finalWinner = "Tie";
+  }
+  return finalWinner;
+}
